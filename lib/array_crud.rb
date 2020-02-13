05871232deaaf57c230e2e1fa08863fdab95f044
Julian Wakefield <julianwakefield@outlook.com>
2020-02-13 13:35:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,6 @@ dogs_array = ["border collie", "poodle", "bull dog", "old english sheepdog"]
 end
 
 def add_element_to_end_of_array(array, element)
-array = dogs_array
-element = "cocker spaniel"
 
 dogs_array << "cocker spaniel"
 
